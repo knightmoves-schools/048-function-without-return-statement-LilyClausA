286589f71807b5b2ee5ad6d0f9c1e974a256febf
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-01-27 20:22:05 +0000

    sure

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let logUser = function(1){
+    log user: ${username}(1)
+}
 
+document.getElementById("result").innerHTML = logUser;
+
+
+//should create a function named logUser that takes one parameter username
+//should log user: ${username} to the console when logUser is called
+//should not return a value when logUser is called
